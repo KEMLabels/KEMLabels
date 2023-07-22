f4e091e35906bb62c1b11fe6e732d6d4265cc26d
Towa Quimbayo <towacurtis@hotmail.com>
2023-07-22 03:51:43 -0700

    completed verify email confirmed page

--- frontend/src/pages/VerifyEmailConfirmation.js
+++ frontend/src/pages/VerifyEmailConfirmation.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "../api/axios";
+import { FaCheckCircle, FaExclamationCircle } from "react-icons/fa";
+import PageLayout from "../components/PageLayout";
+import Button from "../components/Button";
 
 function VerifyEmailConfirmation() {
   const [validURL, setvalidURL] = useState(false);
@@ -17,7 +20,65 @@ function VerifyEmailConfirmation() {
       .catch((err) => setvalidURL(false));
   }, [param]);
 
-  return <div>{validURL ? <h1>EMAIL VERIFIED</h1> : <h1>NOT FOUND</h1>}</div>;
+  return (
+    <PageLayout title={validURL ? "Email Verified" : "Email Link Expired"}>
+      <div
+        className="authContainer"
+        style={{
+          minHeight: "auto",
+          justifyContent: "center",
+          paddingTop: "5rem",
+        }}
+      >
+        <div className="authColumn">
+          <div className="authHeader">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                marginBottom: "1.5rem",
+              }}
+            >
+              {validURL ? (
+                <FaCheckCircle size={80} color="#00cc66" />
+              ) : (
+                <FaExclamationCircle size={80} color="#ff0033" />
+              )}
+            </div>
+            {validURL ? (
+              <>
+                <h1 style={{ textAlign: "center" }}>
+                  Your email has been verified!
+                </h1>
+                <p style={{ opacity: "0.7", textAlign: "center" }}>
+                  Thank you for confirming your email address! You're all set to
+                  begin your purchase.
+                </p>
+              </>
+            ) : (
+              <>
+                <h1 style={{ textAlign: "center" }}>
+                  Your email link has expired!
+                </h1>{" "}
+                <p style={{ opacity: "0.7", textAlign: "center" }}>
+                  We're sorry, but your email link has expired. Please{" "}
+                  <Link to="/verifyemail" className="link">
+                    request a new link here.
+                  </Link>
+                </p>
+              </>
+            )}
+          </div>
+          <Button
+            btnType="button"
+            text="Return to home"
+            onClickEvent={() => (window.location.href = "/")}
+            customStyle={{ width: "100%" }}
+          />
+        </div>
+      </div>
+    </PageLayout>
+  );
 }
 
 export default VerifyEmailConfirmation;
