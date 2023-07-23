9c55c69563ce339fa70d05384a557ad393cf19d1
Towa Quimbayo <towacurtis@hotmail.com>
2023-07-23 00:45:26 -0700

    added how it works section

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,12 +22,39 @@ export default function Home() {
         </div>
       </div>
       {/* TOOD: Add how it works section */}
-      {/* <div className="homeSecContainer" id="howitworks">
+      <div className="homeSecContainer" id="howitworks">
         <h1>How It Works</h1>
-      </div> */}
+        <p>Get started in just a few simple steps.</p>
+        <div className="cardsContainer">
+          <div className="card">
+            <img src="/media/howitworks-step1.jpg" alt="Fund your account illutration" />
+            <div className="cardContent">
+              <h2>Fund your account</h2>
+              <p>Load your credits by making a deposit through Stripe or Crypto.</p>
+            </div>
+          </div>
+          <div className="card reverse">
+            <img src="/media/howitworks-step2.svg" alt="Adding an item to cart illustration" />
+            <div className="cardContent">
+              <h2>Select a shipping label</h2>
+              <p>Choose then add a shipping label item to your cart.</p>
+            </div>
+          </div>
+          <div className="card">
+            <img src="/media/howitworks-step3.jpg" alt="Delivery man illustration" />
+            <div className="cardContent">
+              <h2>Receive your shipping label</h2>
+              <p>Once payment is processed, check your email inbox to receive your shipping label!</p>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="homeSecContainer" id="faq">
         <h1>Frequently Asked Questions</h1>
-        <p>If you have any other questions not covered here, please feel free to contact us, and we'll be happy to assist you.</p>
+        <p>
+          If you have any other questions not covered here, please feel free to
+          contact us, and we'll be happy to assist you.
+        </p>
         <Accordion value={FaqJson}></Accordion>
       </div>
     </PageLayout>
