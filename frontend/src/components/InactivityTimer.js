672f8d618099e5b5e352a16a1732bbc0ec8a6321
Towa Quimbayo <towacurtis@hotmail.com>
2023-07-29 03:04:41 -0700

    added inactivity timer that ends user session after 10 mins

---
+++ frontend/src/components/InactivityTimer.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { clearSession } from "../redux/actions/UserAction";
+import axios from "../api/axios";
+
+export default function InactivityTimer() {
+  const dispatch = useDispatch();
+  const inactivityThreshold = 600000; // 10 minutes (600000 milliseconds)
+  const [lastActivityTime, setLastActivityTime] = useState(Date.now());
+
+  useEffect(() => {
+    const checkInactivity = async () => {
+      const currentTime = Date.now();
+      if (currentTime - lastActivityTime > inactivityThreshold) {
+        await axios.get("/logout", { withCredentials: true });
+        dispatch(clearSession()); // Dispatch logout action after inactivity
+        window.location.reload();
+      }
+    };
+
+    const resetActivityTimer = () => {
+      setLastActivityTime(Date.now());
+    };
+
+    const intervalId = setInterval(checkInactivity, 1000); // Check every second
+
+    // Reset the activity timer when there is user activity (e.g., mousemove, keydown)
+    window.addEventListener("mousemove", resetActivityTimer);
+    window.addEventListener("keydown", resetActivityTimer);
+
+    return () => {
+      clearInterval(intervalId);
+      window.removeEventListener("mousemove", resetActivityTimer);
+      window.removeEventListener("keydown", resetActivityTimer);
+    };
+  }, [dispatch, inactivityThreshold, lastActivityTime]);
+
+  return null; // This component doesn't render anything
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store, persistor } from "./redux/Store";
+import InactivityTimer from "./components/InactivityTimer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <InactivityTimer />
+      <App />
+    </PersistGate>
+  </Provider>
 );
