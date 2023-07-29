3ddaabc349d437bd235b3f32816fcaa00431a392
Towa Quimbayo <towacurtis@hotmail.com>
2023-07-29 01:46:37 -0700

    fixed verify loading bug

--- frontend/src/pages/VerifyEmailConfirmation.js
+++ frontend/src/pages/VerifyEmailConfirmation.js
@@ -9,6 +9,7 @@ import AlertMessage from "../components/AlertMessage";
 export default function VerifyEmailConfirmation() {
   const param = useParams();
 
+  const [fetching, setIsFetching] = useState(true);
   const [loading, setLoading] = useState(false);
   const [validURL, setvalidURL] = useState(false);
   const [errMsg, setErrMsg] = useState("");
@@ -26,6 +27,9 @@ export default function VerifyEmailConfirmation() {
         console.log("Error: ", e);
         setErrMsg(`${e.name}: ${e.message}`);
       });
+    setTimeout(() => {
+      setIsFetching(false);
+    }, 500);
   }, [param]);
 
   return (
@@ -38,59 +42,61 @@ export default function VerifyEmailConfirmation() {
           paddingTop: "5rem",
         }}
       >
-        <div className="authColumn">
-          <div className="authHeader">
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                marginBottom: "1.5rem",
-              }}
-            >
+        {fetching ? null : (
+          <div className="authColumn">
+            <div className="authHeader">
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginBottom: "1.5rem",
+                }}
+              >
+                {validURL ? (
+                  <FaCheckCircle size={80} color="#00cc66" />
+                ) : (
+                  <FaExclamationCircle size={80} color="#ff0033" />
+                )}
+              </div>
               {validURL ? (
-                <FaCheckCircle size={80} color="#00cc66" />
+                <>
+                  <h1 style={{ textAlign: "center" }}>
+                    Your email has been verified!
+                  </h1>
+                  <p style={{ opacity: "0.7", textAlign: "center" }}>
+                    Thank you for confirming your email address! You're all set
+                    to begin your purchase.
+                  </p>
+                </>
               ) : (
-                <FaExclamationCircle size={80} color="#ff0033" />
+                <>
+                  <h1 style={{ textAlign: "center" }}>
+                    Your email link has expired!
+                  </h1>{" "}
+                  <p style={{ opacity: "0.7", textAlign: "center" }}>
+                    We're sorry, but your email link has expired. Please{" "}
+                    <Link to="/verifyemail" className="link">
+                      request a new link here.
+                    </Link>
+                  </p>
+                </>
               )}
             </div>
-            {validURL ? (
-              <>
-                <h1 style={{ textAlign: "center" }}>
-                  Your email has been verified!
-                </h1>
-                <p style={{ opacity: "0.7", textAlign: "center" }}>
-                  Thank you for confirming your email address! You're all set to
-                  begin your purchase.
-                </p>
-              </>
-            ) : (
-              <>
-                <h1 style={{ textAlign: "center" }}>
-                  Your email link has expired!
-                </h1>{" "}
-                <p style={{ opacity: "0.7", textAlign: "center" }}>
-                  We're sorry, but your email link has expired. Please{" "}
-                  <Link to="/verifyemail" className="link">
-                    request a new link here.
-                  </Link>
-                </p>
-              </>
-            )}
+            {errMsg && <AlertMessage msg={errMsg} type="error" />}
+            <Button
+              btnType="button"
+              text="Return to home"
+              loading={loading}
+              onClickEvent={() => {
+                setLoading(true);
+                setTimeout(() => {
+                  window.location.href = "/";
+                }, 100);
+              }}
+              customStyle={{ width: "100%" }}
+            />
           </div>
-          {errMsg && <AlertMessage msg={errMsg} type="error" />}
-          <Button
-            btnType="button"
-            text="Return to home"
-            loading={loading}
-            onClickEvent={() => {
-              setLoading(true);
-              setTimeout(() => {
-                window.location.href = "/";
-              }, 100);
-            }}
-            customStyle={{ width: "100%" }}
-          />
-        </div>
+        )}
       </div>
     </PageLayout>
   );
