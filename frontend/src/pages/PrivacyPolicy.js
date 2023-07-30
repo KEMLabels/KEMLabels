a8b47072f8c371e38d45ca6f284535d9bca1ef15
Towa Quimbayo <towacurtis@hotmail.com>
2023-07-30 03:08:36 -0700

    Formatted privacy and terms pages

--- frontend/src/pages/PrivacyPolicy.js
+++ frontend/src/pages/PrivacyPolicy.js
@@ -1,45 +1,116 @@
-import React from 'react'
-import PageLayout from '../components/PageLayout'
+import React from "react";
+import PageLayout from "../components/PageLayout";
+import "../styles/Global.css";
 
 export default function PrivacyPolicy() {
-    return (
-        <PageLayout title="Privacy Policy">
-        <div style={{padding:'0 5em'}}>
-            <h1>Effective Date: July 29, 2023</h1>
-            <p>At KEMLabels, we are dedicated to safeguarding the privacy and security of our customers' personal information. This Privacy Policy outlines how we collect, use, disclose, and protect your data when you interact with our services. Please take a moment to read this policy carefully.</p>
-
-            <h1>Information We Collect:</h1>
-            <h2>We may collect the following types of information when you use our services:</h2>
-            <p>Tracking Information: When you access our website or use our mobile applications, we may automatically collect certain information, such as your IP address, browser type, operating system, and other technical data. This data helps us improve our services and ensure security.</p>
-
-            <h2>How We Use Your Information:</h2>
-            <ol>
-                <li>Providing Services: We use your personal information to process and fulfill your orders, provide shipping and tracking services, and manage your KEMLabels account.</li>
-                <li>Customer Support: Your information enables us to respond to your inquiries, address issues, and enhance our customer support services.</li>
-                <li>Communication: We may send you service-related updates, notifications, and promotional materials relevant to your interactions with KEMLabels.</li>
-            </ol>
-
-            <h2>Your Choices and Rights:</h2>
-            <ol>
-                <li>Account Settings: You can review and update your account information through your KEMLabels account settings.</li>
-                <li>Marketing Communications: You can opt-out of receiving marketing communications from KEMLabels by following the unsubscribe instructions included in the emails or contacting us directly.</li>
-                <li>Data Access and Deletion: You have the right to request access to the personal information we hold about you and request its deletion, subject to applicable legal requirements.</li>
-            </ol>
-
-            <h2>Data Security:</h2>
-            <p>We employ industry-standard security measures to protect your information from unauthorized access, disclosure, alteration, or destruction. However, no method of data transmission over the internet or electronic storage is completely secure, and we cannot guarantee absolute security.</p>
-
-            <h2>Children's Privacy:</h2>
-            <p>KEMLabels services are not intended for children under the age of 13, and we do not knowingly collect personal information from individuals in this age group.</p>
-
-            <h2>Updates to this Policy:</h2>
-            <p>We may update this Privacy Policy from time to time to reflect changes in our practices or legal requirements. We will notify you of any significant changes through prominent notices on our website or by other means.</p>
-
-            <h1>Contact Us:</h1>
-            <p>If you have any questions or concerns regarding this Privacy Policy or your data, please contact us at [contact email/phone number].</p>
-            <br></br>
-            <p>By using KEMLabels services, you consent to the practices described in this Privacy Policy.</p>
+  return (
+    <PageLayout title="Privacy Policy">
+      <div className="globalContainer">
+        <div className="headingContainer">
+          <h1>Privacy Policy</h1>
+          <p>Effective Date: July 30, 2023</p>
         </div>
-        </PageLayout>
-    )
+
+        <p>
+          At KEMLabels, we are dedicated to safeguarding the privacy and
+          security of our customers' personal information. This Privacy Policy
+          outlines how we collect, use, disclose, and protect your data when you
+          interact with our services. Please take a moment to read this policy
+          carefully.
+        </p>
+
+        <h2>Information We Collect:</h2>
+        <p>
+          We may collect the following types of information when you use our
+          services:
+        </p>
+        <p>
+          Tracking Information: When you access our website or use our mobile
+          applications, we may automatically collect certain information, such
+          as your IP address, browser type, operating system, and other
+          technical data. This data helps us improve our services and ensure
+          security.
+        </p>
+
+        <h2>How We Use Your Information:</h2>
+        <ul>
+          <li>
+            Providing Services: We use your personal information to process and
+            fulfill your orders, provide shipping and tracking services, and
+            manage your KEMLabels account.
+          </li>
+          <li>
+            Customer Support: Your information enables us to respond to your
+            inquiries, address issues, and enhance our customer support
+            services.
+          </li>
+          <li>
+            Communication: We may send you service-related updates,
+            notifications, and promotional materials relevant to your
+            interactions with KEMLabels.
+          </li>
+        </ul>
+
+        <h2>Your Choices and Rights:</h2>
+        <ul>
+          <li>
+            Account Settings: You can review and update your account information
+            through your KEMLabels account settings.
+          </li>
+          <li>
+            Marketing Communications: You can opt-out of receiving marketing
+            communications from KEMLabels by following the unsubscribe
+            instructions included in the emails or contacting us directly.
+          </li>
+          <li>
+            Data Access and Deletion: You have the right to request access to
+            the personal information we hold about you and request its deletion,
+            subject to applicable legal requirements.
+          </li>
+        </ul>
+
+        <h2>Data Security:</h2>
+        <p>
+          We employ industry-standard security measures to protect your
+          information from unauthorized access, disclosure, alteration, or
+          destruction. However, no method of data transmission over the Internet
+          or electronic storage is completely secure, and we cannot guarantee
+          absolute security.
+        </p>
+
+        <h2>Children's Privacy:</h2>
+        <p>
+          KEMLabels services are not intended for children under the age of 13,
+          and we do not knowingly collect personal information from individuals
+          in this age group.
+        </p>
+
+        <h2>Updates to this Policy:</h2>
+        <p>
+          We may update this Privacy Policy from time to time to reflect changes
+          in our practices or legal requirements. We will notify you of any
+          significant changes through prominent notices on our website or by
+          other means.
+        </p>
+
+        <h2>Contact Us:</h2>
+        <p>
+          If you have any questions or concerns regarding this Privacy Policy or
+          your data, please contact us at{" "}
+          <a href="tel:6041231234" className="link">
+            6041231234
+          </a>{" "}
+          or{" "}
+          <a href="mailto:kemlabels@gmail.com" className="link">
+            kemlabels@gmail.com
+          </a>
+          .
+        </p>
+        <p>
+          By using KEMLabels services, you consent to the practices described in
+          this Privacy Policy.
+        </p>
+      </div>
+    </PageLayout>
+  );
 }
--- frontend/src/pages/TermsAndConditions.js
+++ frontend/src/pages/TermsAndConditions.js
@@ -1,44 +1,93 @@
-import React from 'react'
-import PageLayout from '../components/PageLayout'
+import React from "react";
+import PageLayout from "../components/PageLayout";
+import "../styles/Global.css";
 
 function TermsAndConditions() {
-    return (
-        <PageLayout title="Terms and Conditions">
-        <div style={{padding:'0 5em'}}>
-            <h1>Effective Date: July 29, 2023</h1>
-
-            <p>Welcome to KEMLabels! By using our website, mobile applications, and services, you agree to these Terms and Conditions. Please review them before using our services.</p>
-
-            <h1>Use of Services:</h1>
-            <ol>
-                <li>Age Requirement: You must be at least 18 years old to use our services.</li>
-                <li> Account: If needed, you may create an account. Keep your login details secure and do not share them.</li>
-                <li>Respectful Use: Use our services responsibly and refrain from posting offensive or harmful content.</li>
-            </ol>
-
-            <h1>Content:</h1>
-            <ol>
-                <li>Your Content: By submitting content, you grant KEMLabels permission to use it for our services.</li>
-                <li>Prohibited Content: Do not submit content that violates laws or others' rights.</li>
-            </ol>
-
-            <h1>Third-Party Links:</h1>
-            <p>We are not responsible for third-party websites or services linked from our site.</p>
-
-            <h1>Liability:</h1>
-
-            <p>We strive to provide accurate and reliable services, but we cannot guarantee everything. KEMLabels is not liable for any damages resulting from using our services.</p>
-
-            <h1>Indemnification:</h1>
-            <p>You agree to hold KEMLabels harmless from any claims or losses due to your use of our services.</p>
-
-            <h1>Governing Law:</h1>
-            <p>These Terms are governed by the laws of Canada.</p>
-            <br></br>
-            <p>By using KEMLabels services, you accept these Terms and Conditions. If you have any questions, contact us at [contact email/phone number].</p>
+  return (
+    <PageLayout title="Terms and Conditions">
+      <div className="globalContainer">
+        <div className="headingContainer">
+          <h1>Terms and Conditions</h1>
+          <p>Effective Date: July 30, 2023</p>
         </div>
-        </PageLayout>
-    )
+
+        <p>
+          Welcome to KEMLabels! By using our website, mobile applications, and
+          services, you agree to these Terms and Conditions. Please review them
+          before using our services.
+        </p>
+
+        <h2>Use of Services:</h2>
+        <ul>
+          <li>
+            Age Requirement: You must be at least 18 years old to use our
+            services.
+          </li>
+          <li>
+            Account: If needed, you may create an account. Keep your login
+            details secure and do not share them.
+          </li>
+          <li>
+            Respectful Use: Use our services responsibly and refrain from
+            posting offensive or harmful content.
+          </li>
+        </ul>
+
+        <h2>Content:</h2>
+        <ul>
+          <li>
+            Your Content: By submitting content, you grant KEMLabels permission
+            to use it for our services.
+          </li>
+          <li>
+            Prohibited Content: Do not submit content that violates laws or
+            others' rights.
+          </li>
+        </ul>
+
+        <h2>Third-Party Links:</h2>
+        <p>
+          We are not responsible for third-party websites or services linked
+          from our site.
+        </p>
+
+        <h2>Liability:</h2>
+
+        <p>
+          We strive to provide accurate and reliable services, but we cannot
+          guarantee everything. KEMLabels is not liable for any damages
+          resulting from using our services.
+        </p>
+
+        <h2>Indemnification:</h2>
+        <p>
+          You agree to hold KEMLabels harmless from any claims or losses due to
+          your use of our services.
+        </p>
+
+        <h2>Governing Law:</h2>
+        <p>These Terms are governed by the laws of Canada.</p>
+
+        <h2>Contact Us:</h2>
+        <p>
+          If you have any questions or concerns regarding these Terms, please
+          contact us at{" "}
+          <a href="tel:6041231234" className="link">
+            6041231234
+          </a>{" "}
+          or{" "}
+          <a href="mailto:kemlabels@gmail.com" className="link">
+            kemlabels@gmail.com
+          </a>
+          .
+        </p>
+
+        <p>
+          By using KEMLabels services, you accept these Terms and Conditions.
+        </p>
+      </div>
+    </PageLayout>
+  );
 }
 
-export default TermsAndConditions
+export default TermsAndConditions;
