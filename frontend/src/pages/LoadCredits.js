f26f7584ba04f97c3c96de7649103834a1d6886d
Towa Quimbayo <towacurtis@hotmail.com>
2023-08-01 01:36:24 -0700

    completed load credits page

--- frontend/src/pages/LoadCredits.js
+++ frontend/src/pages/LoadCredits.js
@@ -1,32 +1,65 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import axios from "../api/axios";
-import { InputField } from "../components/Field";
-import Button from "../components/Button";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import PageLayout from "../components/PageLayout";
+import "../styles/Global.css";
+import "../styles/LoadCredits.css";
+import { FaBitcoin, FaRegCreditCard } from "react-icons/fa";
 
 export default function LoadCredits() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
-  //make sure user is logged in
-  //store amount and send to credit card or coinbase
+  useEffect(() => {
+    if (!isLoggedIn) window.location.href = "/";
+  }, [isLoggedIn]);
 
   return (
-    <form action="POST">
-      <p>Wishing to deposit how much?</p>
-      <InputField
-        fieldType="number"
-        placeholder="Amount in USD"
-      />
+    <PageLayout title="Load Credits">
+      <div
+        className="authContainer"
+        style={{
+          minHeight: "auto",
+          justifyContent: "center",
+          paddingTop: "5rem",
+        }}
+      >
+        <div className="authColumn" style={{ width: "100%" }}>
+          <div className="authHeader">
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <img
+                src="/media/paymentoption.jpg"
+                alt="Payment option illustration"
+                width="100%"
+                style={{ maxWidth: "400px" }}
+              />
+            </div>
+            <h1 style={{ textAlign: "center" }}>How do you want to load?</h1>
+            <p style={{ opacity: "0.7", textAlign: "center" }}>
+              Please choose one of the payment options below to get started.
+            </p>
+          </div>
 
-      <Button
-        btnType="submit"
-        text="Credit Card"
-        customStyle={{ marginTop: "1rem" }}
-      />
-      <Button
-        btnType="submit"
-        text="Crypto Currency"
-        customStyle={{ marginTop: "1rem" }}
-      />
-    </form>
-  )
+          <div className="paymentOptionCardGroup">
+            <div
+              className="paymentOptionCard"
+              onClick={() => {
+                window.location.href = "/pay/creditcard";
+              }}
+            >
+              <p>Credit / Debit Card</p>
+              <FaRegCreditCard />
+            </div>
+            <div
+              className="paymentOptionCard"
+              onClick={() => {
+                window.location.href = "/pay/crypto";
+              }}
+            >
+              <p>Crypto Currency</p>
+              <FaBitcoin />
+            </div>
+          </div>
+        </div>
+      </div>
+    </PageLayout>
+  );
 }
