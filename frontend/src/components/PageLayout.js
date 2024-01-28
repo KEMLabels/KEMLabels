d43e70e8c103b25d7f0e896caceb2241a2f2f599
Towa Quimbayo <towacurtis@hotmail.com>
2024-01-28 03:28:33 -0800

    ud meta data

--- frontend/src/components/PageLayout.js
+++ frontend/src/components/PageLayout.js
@@ -16,9 +16,8 @@ export default function PageLayout({
   return (
     <>
       <Helmet>
-        <title>
-          {isLandingPage ? "KEMLabels" : `${title} | KEMLabels`}
-        </title>
+        <meta charSet="utf-8" />
+        <title>{isLandingPage ? "KEMLabels" : `${title} | KEMLabels`}</title>
         {description && <meta name="description" content={description} />}
       </Helmet>
       <Navbar hideNavAndFooter={hideNavAndFooter} />
