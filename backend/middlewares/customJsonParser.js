ad4b4d2999c3cc20cdefcc98abdf45775cb94c7c
Towa Quimbayo <towacurtis@hotmail.com>
2024-05-31 02:16:12 -0700

    fixed custom JSONparser path for webhooks

--- backend/middlewares/customJsonParser.js
+++ backend/middlewares/customJsonParser.js
@@ -3,8 +3,8 @@ const express = require("express");
 function customJsonParser(req, res, next) {
   // If the request is for "/crypto/webhook", skip the JSON parsing
   if (
-    (req.path === "/crypto/webhook" && req.method === "POST") ||
-    (req.path === "/webhook" && req.method === "POST")
+    (req.path === "/api/v1/payment/crypto/webhook" && req.method === "POST") ||
+    (req.path === "/api/v1/payment/webhook" && req.method === "POST")
   ) {
     express.raw({ type: "application/json" })(req, res, next);
   } else express.json()(req, res, next); // For all other requests, use express.json()
